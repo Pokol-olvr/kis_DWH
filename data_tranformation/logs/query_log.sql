74bc4a0364e76d2673938cd5fa7df37a9169226e
Pokol <petrenyio@gmail.com>
2026-04-01 22:09:11 +0200

    dbt init

---
+++ data_tranformation/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-04-01T20:08:46.656630800+00:00
+-- finished_at: 2026-04-01T20:08:51.095372900+00:00
+-- elapsed: 4.4s
+-- outcome: success
+-- dialect: bigquery
+-- node_id: not available
+-- query_id: 1k6wKlSPA6HuWAOQfsgzephr7Qr
+-- desc: not provided
+select 1 as id;
